a73e882ff26c11d444e831f0db8e98ab23a0bb0e
Loïck Francilien <loic@unkorneglosk.fr>
2024-02-15 00:15:13 +0100

    More tests for PHP7.4

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,24 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.1"
 
-extra_php_dependencies="php${YNH_PHP_VERSION} php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl  php${YNH_PHP_VERSION}-cli"
+php_dependencies="php$YNH_PHP_VERSION-mysql \
+						php$YNH_PHP_VERSION-imagick \
+						php$YNH_PHP_VERSION-gd \
+						php$YNH_PHP_VERSION-mbstring \
+						php$YNH_PHP_VERSION-soap \
+						php$YNH_PHP_VERSION-curl \
+						php$YNH_PHP_VERSION-intl \
+						php$YNH_PHP_VERSION-opcache \
+						php$YNH_PHP_VERSION-calendar \
+						php$YNH_PHP_VERSION-zip \
+						php$YNH_PHP_VERSION-xml \
+						php$YNH_PHP_VERSION-fileinfo \
+						php$YNH_PHP_VERSION-imap"
+
+# dependencies used by the app (must be on a single line)
+pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
